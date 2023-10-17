fe0f4c80d0090a47e61b3472e9f44437de65a82c
Raí Gumieri dos Santos <127215645+raigumieri@users.noreply.github.com>
2023-10-17 18:59:42 -0300

    Versão 7

--- src/components/ListaProdutos.jsx
+++ src/components/ListaProdutos.jsx
@@ -1,7 +1,7 @@
 export const ListaProdutos =[
-    {id: 1, nome: "Onigiri", desc: "Bolinho de arroz", valor: 6.80},
-    {id: 2, nome: "Uramaki", desc: "", valor: 19.99},
+    {id: 1, nome: "Onigiri", desc: "Bolinho de arroz", valor: 6.89},
+    {id: 2, nome: "Uramaki", desc: "Tipo de sushi", valor: 19.99},
     {id: 3, nome: "Temaki", desc: "Temaki de salmao com cebolinha", valor: 19.39},
-    {id: 4, nome: "Hot Roll", desc: "hot roll com molho doce ", valor: 24.49},
-    {id: 5, nome: "Sashimi", desc: "um conjunto de peixes ", valor: 48.78},
-    {id: 6, nome: "Shimeji", desc: "cogumelo frito ", valor: 21.79},]
+    {id: 4, nome: "Hot Roll", desc: "Hot roll com molho doce ", valor: 24.49},
+    {id: 5, nome: "Sashimi", desc: "Conjunto de peixes ", valor: 48.78},
+    {id: 6, nome: "Shimeji", desc: "Cogumelo frito ", valor: 21.79},]
